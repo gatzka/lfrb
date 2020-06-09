108964115cf7b56db9eedb807ae3cc7b505c1e69
gatzka <gatzka@users.noreply.github.com>
2020-06-09 14:45:37 +0000

    Deploy to GitHub Pages

--- doc/html/lfrb_8h.js
+++ doc/html/lfrb_8h.js
@@ -7,14 +7,14 @@ var lfrb_8h =
       [ "LFRB_INVALID_ARGUMENT", "lfrb_8h.html#acd1fe3b16fcc7d17d6e1ecb9ca49148ea608e5a07d37c349bee2193e6584da0b4", null ]
     ] ],
     [ "lfrb_dequeue", "lfrb_8h.html#a949e6f63bb4512610ac1e74cedd8c61a", null ],
-    [ "lfrb_dequeue_available", "lfrb_8h.html#afde8465bc51b9015e9a6c146064bbe4f", null ],
-    [ "lfrb_dequeuer_clear", "lfrb_8h.html#adb1d26b1cbfc0631cb6eed86b1028a25", null ],
+    [ "lfrb_dequeue_available", "lfrb_8h.html#ab65268272ec46e1d83f65932a5c2144d", null ],
+    [ "lfrb_dequeuer_clear", "lfrb_8h.html#a767f22407e2c34b1403be82523929afb", null ],
     [ "lfrb_enqueue", "lfrb_8h.html#a50c1f9d5361a506dbe1b291a3b7310a0", null ],
-    [ "lfrb_enqueue_available", "lfrb_8h.html#aaf417d93fd9a811ffe252033d9361633", null ],
-    [ "lfrb_enqueue_buffer", "lfrb_8h.html#a1103c4cd22dc73f2cfba693004b32a14", null ],
-    [ "lfrb_enqueuer_clear", "lfrb_8h.html#ab42314bc61ddf93b37296292d5c76864", null ],
-    [ "lfrb_init", "lfrb_8h.html#a7d80f245352758222a8e6871af47a392", null ],
-    [ "lfrb_is_empty", "lfrb_8h.html#a361c5e3efcf835cb6b0d18de5649a6ba", null ],
-    [ "lfrb_is_full", "lfrb_8h.html#a7fd8669bd41fd81a28b6ba9cb73bc74d", null ],
-    [ "lfrb_size", "lfrb_8h.html#af9e3e0f930a9aa941140b0332fb646ca", null ]
+    [ "lfrb_enqueue_available", "lfrb_8h.html#ae9a2442d73a41303bd4b079bac489171", null ],
+    [ "lfrb_enqueue_buffer", "lfrb_8h.html#adb2e79b9cd6adb90f3721a4f72b3a929", null ],
+    [ "lfrb_enqueuer_clear", "lfrb_8h.html#a805f4d54c7eb984e11d66250fb15c604", null ],
+    [ "lfrb_init", "lfrb_8h.html#af73910afcd5eb2cf00574c94a25d090b", null ],
+    [ "lfrb_is_empty", "lfrb_8h.html#aa0dd06a847bba1dd16b8e95f27c100f8", null ],
+    [ "lfrb_is_full", "lfrb_8h.html#a941613e633cdebab0069065449dfde34", null ],
+    [ "lfrb_size", "lfrb_8h.html#a1703f4ea509c3c741cc1d10dc5e492e4", null ]
 ];
